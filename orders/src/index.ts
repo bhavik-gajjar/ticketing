4084614efa733bf08676e928ce75c4eef516f6ba
bhavik-gajjar <bhavik.bhalara@rapidops.com>
2023-05-04 16:25:11 +0530

    Small Change

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,8 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
-  console.log('Start Orders');
+  console.log('Start Orders..');
+  console.log('Start Orders..');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
