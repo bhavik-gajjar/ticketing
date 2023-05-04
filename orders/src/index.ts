775dda737d2d5db682b850447df12273bf994d53
bhavik-gajjar <bhavik.bhalara@rapidops.com>
2023-05-04 16:27:02 +0530

    Small Changes in auth

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,6 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 
 const start = async () => {
   console.log('Start Orders..');
-  console.log('Start Orders..');
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
